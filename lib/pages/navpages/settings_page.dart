179fab86b18c65108dbea7a369f7479e0cda4cd8
ekyut123 <86719994+ekyut123@users.noreply.github.com>
2022-11-10 00:53:43 +0800

    Add files via upload

---
+++ lib/pages/navpages/settings_page.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class SettingsPage extends StatefulWidget {
+  static const String path = "lib/src/pages/settings/settings1.dart";
+
+  const SettingsPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SettingsPage createState() => _SettingsPage();
+}
+
+class _SettingsPage extends State<SettingsPage> {
+  late bool _dark;
+  bool toggle = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _dark = false;
+  }
+
+  Brightness _getBrightness() {
+    return _dark ? Brightness.dark : Brightness.light;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+        brightness: _getBrightness(),
+      ),
+      child: Scaffold(
+        backgroundColor: _dark ? null : Colors.grey.shade200,
+        appBar: AppBar(
+          elevation: 0,
+          // ignore: deprecated_member_use
+          brightness: _getBrightness(),
+          iconTheme: IconThemeData(color: _dark ? Colors.white : Colors.black),
+          backgroundColor: Colors.deepOrange,
+          title: Text(
+            'Settings',
+            style: TextStyle(color: _dark ? Colors.white : Colors.black),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.brightness_2),
+              onPressed: () {
+                setState(() {
+                  _dark = !_dark;
+                });
+              },
+            )
+          ],
+        ),
+        body: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Card(
+                    elevation: 8.0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    color: Colors.deepOrange,
+                    child: ListTile(
+                      onTap: () {
+                        //open edit profile
+                      },
+                      title: const Text(
+                        "Jericho",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      trailing: const Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  Card(
+                    elevation: 4.0,
+                    margin: const EdgeInsets.fromLTRB(32.0, 8.0, 32.0, 16.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Column(
+                      children: <Widget>[
+                        ListTile(
+                          leading: const Icon(
+                            Icons.lock_outline,
+                            color: Colors.deepOrange,
+                          ),
+                          title: const Text("Change Password"),
+                          trailing: const Icon(Icons.keyboard_arrow_right),
+                          onTap: () {
+                            //open change password
+                          },
+                        ),
+                        _buildDivider(),
+                        ListTile(
+                          leading: const Icon(
+                            Icons.language,
+                            color: Colors.deepOrange,
+                          ),
+                          title: const Text("Change Language"),
+                          trailing: const Icon(Icons.keyboard_arrow_right),
+                          onTap: () {
+                            //open change language
+                          },
+                        ),
+                        _buildDivider(),
+                        ListTile(
+                          leading: const Icon(
+                            Icons.location_on,
+                            color: Colors.deepOrange,
+                          ),
+                          title: const Text("Change Location"),
+                          trailing: const Icon(Icons.keyboard_arrow_right),
+                          onTap: () {
+                            //open change location
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  const Text(
+                    "Notification Settings",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.indigo,
+                    ),
+                  ),
+                  SwitchListTile(
+                    activeColor: Colors.deepOrange,
+                    contentPadding: const EdgeInsets.all(0),
+                    value: toggle,
+                    title: const Text("Received notification"),
+                    onChanged: (bool value) {
+                      setState(() {
+                        toggle = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _buildDivider() {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 8.0,
+      ),
+      width: double.infinity,
+      height: 1.0,
+      color: Colors.grey.shade400,
+    );
+  }
+}
